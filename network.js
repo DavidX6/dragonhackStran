075d40de0e31f8a5102c0cab609c34c83dad8e20
DavidX6 <david.miskic@gmail.com>
2019-05-18 19:50:22 +0200

    Add files via upload

---
+++ network.js
@@ -0,0 +1,4 @@
+/*global $*/
+$.get("http://169.254.161.196:8888/getbalance", function (data) {
+    console.log(data)
+});
---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'))
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+  //call this app from https://<workspace name>-<user name>.c9users.io
+});
